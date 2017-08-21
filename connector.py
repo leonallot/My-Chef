39998a355c725b46b93d8e45e130f57a9ed8318c
Alfred <leonallot@gmail.com>
2017-08-21 04:20:24 +0000

    pushed 20-08-2017

--- connector.py
+++ connector.py
@@ -2,19 +2,46 @@ import MySQLdb as db
 
 class DBManager:
 	def __init__(self):
-		db_con = db.connect('localhost','root','carrot24','Chef')
-		self.cursor = db_con.cursor()
-		
+		self.db_con = db.connect('localhost','root','carrot24','Chef')
+		self.cursor = self.db_con.cursor()
+
 	def getMeals(self,category=False):
 		if category:
 			self.cursor.execute("select * from meal where category='%s'"%category)
 		else:
 			self.cursor.execute("select * from meal")
-
 		tray = self.cursor.fetchall()
 		result = []
- 
 		for item in tray:
 			result.append([str(item[0]),str(item[1]),float(item[2]),int(item[3])])
-
 		return result
+
+	#cart connections
+	def addToCart(self,username,item):
+		self.cursor.execute('insert into cart(user_id, meal_id) values ("%s", %s)'%(username,item))
+		self.db_con.commit()
+
+	def getCartContent(self,username):
+		self.cursor.execute('select ml.name, ml.price, ct.quantity from meal as ml, cart as ct where ml.id = ct.meal_id')
+		tray = self.cursor.fetchall()
+                result = []
+                for item in tray:
+                        result.append([str(item[0]),float(item[1]),int(item[2])])
+                return result
+
+	def updateCartQuantity(self, username, quantity):
+		self.cursor.execute('update cart set quantity = %s where meal_id=%s and username="%s"'%(quantity,username))
+		self.db_con.commit()
+
+	def clearCart(self,username):
+		self.cursor.execute('delete from cart where user_id="%s"'%username)
+	
+	def removeItem(self,username,id):
+		self.cursor.execute('delete from cart where user_id="%s" and meal_id=%s'%(username, id))
+
+	#ordering
+	def makeOrder(self):
+		return "Done"
+
+	def __exit__(self):
+		self.db_con.close()
--- main.py
+++ main.py
@@ -4,22 +4,38 @@ import connector
 app = Flask(__name__)
 
 @app.route('/', methods=['Get'])
-def index():
+@app.route('/<string:username>', methods=['Get'])
+def index(username = False):
 	return render_template('home.html')
 
 @app.route('/menu/', methods=['Get'])
-def menu():
+@app.route('/<string:username>/menu/')
+def menu(username=False):
 	breakfast = connector.DBManager().getMeals("BreakFast")
 	salads = connector.DBManager().getMeals("Salads")
-	
-	return render_template('menu.html',breakfast=breakfast, salads=salads)
+	cont_loc = connector.DBManager().getMeals("Continental & Local")
+	local = connector.DBManager().getMeals("Local")
+	saubbq = connector.DBManager().getMeals("Sauce & BBQ")
+	if not username:
+		return render_template('menu1.html',breakfast=breakfast, salads=salads, cont_loc=cont_loc, local=local, saubbq=saubbq)
+	return render_template('menu.html',username=username,breakfast=breakfast, salads=salads, cont_loc=cont_loc, local=local, saubbq=saubbq)
 
 @app.route('/cart/', methods=['Get'])
-def cart():
-	return "display cart content"
+@app.route('/<string:username>/cart/', methods=['Get'])
+def cart(username=False):
+	if not username:
+		return "nothing to do here"
+	content=connector.DBManager().getCartContent(username)
+	return render_template('cart.html')
 
-@app.route('/cart/order', methods=['Post'])
-def order():
+@app.route('/<string:username>/cart/add/<int:item>/', methods=['Get'])
+def addItem(username,item):
+	connector.DBManager().addToCart(username,item)
+	return menu(username)
+
+@app.route('/<string:username>/cart/order', methods=['Post'])
+def order(username):
+	#connector.DBManager().makeOrder(username)
 	return "nothin here now"
 
 
